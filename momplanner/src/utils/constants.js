0a3fb73006607151c9b5414affcca44fb830b4d0
nathaniamnbe <148877283+nathaniamnbe@users.noreply.github.com>
2025-11-18 14:13:42 +0700

    Update constants.js

--- momplanner/src/utils/constants.js
+++ momplanner/src/utils/constants.js
@@ -1,3 +1,3 @@
 // Ganti string di bawah dengan Web App URL kamu
 export const APPS_SCRIPT_URL =
-  "https://script.google.com/macros/s/AKfycbwMf55_YgIxWDVHVI-M_885ejsneec8gxedVIgzn2Ul4QKOId0WNLaNsf7nFy4QNhUd/exec";
+  "https://script.google.com/macros/s/AKfycby9DerIF1BecPZ8V1AyLMCIEBVoiTXh0lTrWOqyXH09ztzaZP0CYCKq5tFzGzuqPJF-/exec";
